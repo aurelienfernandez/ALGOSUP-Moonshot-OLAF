058b9f486986f732db9e1ec0b66bcd578b053155
Aurelien FERNANDEZ <aurelien.fernandez@algosup.com>
2024-05-01 10:52:34 +0200

    Chore: divided the parser into two files
    The parser file has been divided into two parts to make it easier to read and update.

---
+++ olaf/lib/lexica_loader.dart
@@ -0,0 +1,140 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+//------------------------ LEXICA PARSER ------------------------
+Future<void> loadLexica() async {
+  final String jsonString = await rootBundle.loadString('assets/lexica.json');
+  var data = jsonDecode(jsonString);
+
+  // Get version
+  var version = data['version'] ?? '';
+  // Get all plants
+  var plantsJson = (data['plants'] as Map<String, dynamic>)
+      .values
+      .map((x) => LexPlant.fromJson(x))
+      .toList();
+  // Get all diseases
+  var diseaseJson =
+      (data['diseases'] as List).map((json) => Disease.fromJson(json)).toList();
+
+  // Initialize lexica
+  Lexica.initialize(
+    version: version,
+    plants: plantsJson,
+    diseases: diseaseJson,
+  );
+}
+
+//---------------------- PLANT DISEASE ---------------------
+class PlantDisease {
+  final String name;
+  final String image;
+
+  PlantDisease({required this.name, required this.image});
+  factory PlantDisease.fromJson(Map<String, dynamic> json) {
+    return PlantDisease(name: json['name'], image: json['image']);
+  }
+}
+
+//---------------------- LEXICA PLANT ----------------------
+class LexPlant {
+  final String name;
+  final String image;
+  final String howTo;
+  final List<String> tips;
+  final List<PlantDisease> diseases;
+
+  LexPlant({
+    required this.name,
+    required this.image,
+    required this.howTo,
+    required this.tips,
+    required this.diseases,
+  });
+
+  factory LexPlant.fromJson(Map<String, dynamic> json) {
+    List<PlantDisease> diseases = [PlantDisease(name: "name", image: "image")];
+    List<String> tips = (json['tips'] as List<dynamic>).cast<String>().toList();
+
+    return LexPlant(
+        name: json['name'] ?? '',
+        image: json['image'] ?? '',
+        howTo: json['howTo'] ?? '',
+        tips: tips,
+        diseases: diseases);
+  }
+}
+
+//------------------------- DISEASE -------------------------
+class Disease {
+  final String name;
+  final String icon;
+  final String description;
+  final String prevent;
+  final String cure;
+
+  Disease({
+    required this.name,
+    required this.icon,
+    required this.description,
+    required this.prevent,
+    required this.cure,
+  });
+
+  factory Disease.fromJson(Map<String, dynamic> json) {
+    return Disease(
+      name: json['name'] ?? '',
+      icon: json['icon'] ?? '',
+      description: json['description'] ?? '',
+      prevent: json['prevent'] ?? '',
+      cure: json['cure'] ?? '',
+    );
+  }
+}
+
+//------------------------ LEXICA ------------------------
+class Lexica {
+  final String version;
+  final List<LexPlant> plants;
+  final List<Disease> diseases;
+
+  // Private constructor
+  Lexica._({
+    required this.version,
+    required this.plants,
+    required this.diseases,
+  });
+
+  // Static instance field
+  static Lexica? _instance;
+
+  // Static method to access the single instance
+  static Lexica getInstance() {
+    if (_instance == null) {
+      throw Exception("Lexica not initialized. Call initialize() first.");
+    }
+    return _instance!;
+  }
+
+  static void initialize({
+    required String version,
+    required List<LexPlant> plants,
+    required List<Disease> diseases,
+  }) {
+    if (_instance != null) {
+      throw Exception("Lexica already initialized.");
+    }
+    {
+      _instance = Lexica._(
+        version: version,
+        plants: plants,
+        diseases: diseases,
+      );
+    }
+  }
+
+  static void reset() {
+    _instance = null;
+  }
+}
---
+++ olaf/lib/user_loader.dart
@@ -0,0 +1,93 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:olaf/plant_page.dart';
+
+//------------------------- USER PARSER -------------------------
+Future<void> loadUser() async {
+  final String jsonString = await rootBundle.loadString('assets/user.json');
+  var data = jsonDecode(jsonString);
+
+  // Parse user data
+  var userJson = data['user'];
+  User.initialize(
+    username: userJson['name'] ?? '',
+    email: userJson['email'] ?? '',
+    profilePicture: userJson['profilePicture'] ?? '',
+    plants: List<Plant>.from(userJson['plants'].map((x) => Plant.fromJson(x))),
+  );
+}
+
+//------------------------- PLANT -------------------------
+class Plant {
+  final String name;
+  final String image;
+  final String disease;
+  final String maturation;
+  final String soilHumidity;
+  final String airHumidity;
+  final String temperature;
+
+  Plant({
+    required this.name,
+    required this.image,
+    required this.disease,
+    required this.maturation,
+    required this.soilHumidity,
+    required this.airHumidity,
+    required this.temperature,
+  });
+
+  factory Plant.fromJson(Map<String, dynamic> json) {
+    return Plant(
+      name: json['name'] ?? '',
+      image: json['image'] ?? '',
+      disease: json['disease'] ?? '',
+      maturation: json['maturation'] ?? '',
+      soilHumidity: json['soilHumidity'] ?? '',
+      airHumidity: json['airHumidity'] ?? '',
+      temperature: json['temperature'] ?? '',
+    );
+  }
+}
+
+//------------------------- USER -------------------------
+class User {
+  final String username;
+  final String profilePicture;
+  final String email;
+  final List<Plant> plants;
+
+  User._({
+    required this.username,
+    required this.profilePicture,
+    required this.email,
+    required this.plants,
+  });
+
+  static User? _instance;
+
+  static User getInstance() {
+    if (_instance == null) {
+      throw Exception("User not initialized. Call initialize() first.");
+    }
+    return _instance!;
+  }
+
+  static void initialize({
+    required String username,
+    required String profilePicture,
+    required String email,
+    required List<Plant> plants,
+  }) {
+    if (_instance != null) {
+      throw Exception("User already initialized.");
+    }
+    _instance = User._(
+      username: username,
+      email: email,
+      profilePicture: profilePicture,
+      plants: plants,
+    );
+  }
+}
